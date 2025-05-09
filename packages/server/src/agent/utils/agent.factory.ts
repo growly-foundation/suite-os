bf3e599086c15a820807e11ee5faafbcf1874bf0
Minh Pham <pcminh0505@gmail.com>
2025-05-09 11:57:38 +0700

    feat: Add DefiLlama protocol api

--- packages/server/src/agent/utils/agent.factory.ts
+++ packages/server/src/agent/utils/agent.factory.ts
@@ -1,8 +1,9 @@
 // src/langchain/agent/agent.factory.ts
 import { createReactAgent } from '@langchain/langgraph/prebuilt';
-import { ChatModelFactory, ChatProvider } from './model.factory';
-import { makeZerionTools } from '../tools/zerion/zerion';
 import { ConfigService } from '@nestjs/config';
+import { getProtocolTool } from '../tools/defillama/defillama';
+import { makeZerionTools } from '../tools/zerion/zerion';
+import { ChatModelFactory, ChatProvider } from './model.factory';
 
 /**
  * Initializes and returns an instance of the AI agent with a dynamic system prompt.
@@ -23,7 +24,11 @@ export function createAgent(
     const { getPortfolioOverviewTool, getFungiblePositionsTool } =
       makeZerionTools(configService);
 
-    const tools = [getPortfolioOverviewTool, getFungiblePositionsTool];
+    const tools = [
+      getPortfolioOverviewTool,
+      getFungiblePositionsTool,
+      getProtocolTool,
+    ];
 
     // Initialize Agent without checkpointer
     return createReactAgent({
