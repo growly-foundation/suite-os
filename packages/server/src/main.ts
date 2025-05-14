53cbd8d527527b67f3ec679e28b0a3a0acb9d493
Minh Pham <pcminh0505@gmail.com>
2025-05-14 20:12:30 +0700

    feat: Enable trustProxy

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { paymentMiddleware, Resource } from 'x402-express';
 import * as dotenv from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
+
   const facilitatorUrl = process.env.FACILITATOR_URL as Resource;
   const payTo = process.env.ADDRESS as `0x${string}`;
   const network = process.env.NETWORK || 'base-sepolia';
