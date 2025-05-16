1d380bc1ac0bc494dbc95c0e8037221aee671a49
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-05-16 19:04:53 +0700

    fix(dashboard): auth

--- packages/dashboard/components/auth/login-form.tsx
+++ packages/dashboard/components/auth/login-form.tsx
@@ -14,6 +14,12 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) 
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card className="overflow-hidden">
         <CardContent className="grid p-0 md:grid-cols-2 shadow-2xl">
+          <img
+            src="/banners/growly-suite-signin.png"
+            alt="Image"
+            style={{ width: 500, height: 500 }}
+            className="object-cover dark:brightness-[0.2] dark:grayscale"
+          />
           <div className="flex flex-col justify-center items-center p-6 md:p-8 min-h-[500px] gap-6">
             <div className="flex flex-col gap-2 items-center text-center">
               <h1 className="text-2xl font-bold">Welcome to Growly!</h1>
@@ -29,13 +35,6 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) 
               {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : ''}
               {isLoading ? 'Signing in...' : 'Sign in'}
             </Button>
-          </div>
-          <div className="relative hidden bg-muted md:block">
-            <img
-              src="/banners/growly-suite-signin.png"
-              alt="Image"
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-            />
           </div>
         </CardContent>
       </Card>
