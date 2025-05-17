092452beb2b32897a7f6d9da1aa922caf06cc908
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-05-17 12:13:53 +0700

    chore(format): reformat message components

--- packages/suite/src/components/messages/system/error.tsx
+++ packages/suite/src/components/messages/system/error.tsx
@@ -1,18 +1,13 @@
 import { SystemErrorMessageContent } from '@growly/core';
 import { AlertCircle } from 'lucide-react';
 
-export const buildSystemErrorMessage = (
-  content: SystemErrorMessageContent['content'],
-  time: string
-) => {
+export const buildSystemErrorMessage = (content: SystemErrorMessageContent['content']) => {
   return (
     <p className="text-sm">
       <span className="text-red-500 flex items-center space-x-2">
         <AlertCircle className="w-4 h-4 opacity-50 mr-2" />
         {content}
       </span>
-      <br />
-      <span className="text-xs opacity-50">{time}</span>
     </p>
   );
 };
