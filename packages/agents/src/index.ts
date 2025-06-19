5c0427a0c13bd2d6c9e697d2e372969c371f24d7
Minh Pham <pcminh0505@gmail.com>
2025-06-19 14:07:00 +0700

    feat(agents): Add text recommendation

--- packages/agents/src/index.ts
+++ packages/agents/src/index.ts
@@ -1,13 +1,16 @@
 // Export all tools
-export * from './tools/tavily';
-export * from './tools/zerion';
 export * from './tools/defillama';
+export * from './tools/tavily';
 export * from './tools/uniswap';
+export * from './tools/zerion';
 
 // Export utils
 export * from './utils/agent.factory';
-export * from './utils/model.factory';
 export * from './utils/checkpointer';
+export * from './utils/model.factory';
+
+// Export services
+export * from './services/recommendation.service';
 
 // Export prompt
 export * from './prompt';
