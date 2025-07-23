20c0b7e10cd26fda47e9f8e49abd9b78c48db336
Minh Pham <pcminh0505@gmail.com>
2025-07-23 16:46:46 +0700

    feat(server,dashboard): website extractor with Firecrawl

--- apps/server/src/modules/resources/resources.module.ts
+++ apps/server/src/modules/resources/resources.module.ts
@@ -4,11 +4,20 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '../databases/database.module';
 import { SupabaseModule } from '../databases/supabase.module';
 import { EtherscanModule } from '../etherscan/etherscan.module';
+import { FirecrawlModule } from '../firecrawl/firecrawl.module';
+import { OpenAIModule } from '../openai/openai.module';
 import { ResourcesController } from './resources.controller';
 import { ResourcesService } from './resources.service';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, SupabaseModule, EtherscanModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    SupabaseModule,
+    EtherscanModule,
+    FirecrawlModule,
+    OpenAIModule,
+  ],
   controllers: [ResourcesController],
   providers: [ResourcesService],
   exports: [ResourcesService],
