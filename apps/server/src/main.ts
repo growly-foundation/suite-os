a6b41ade4f64040d35208cf24ad5db0dfe96c1e4
Minh Pham <pcminh0505@gmail.com>
2025-07-28 22:13:30 +0700

    feat(server): add class-validator

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from 'dotenv';
@@ -46,6 +47,7 @@ async function bootstrap() {
   // );
 
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 8080);
 }
 
