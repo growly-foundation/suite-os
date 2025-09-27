5ef69fe980d60b7d8611e951d13abc1e3fd2ebe5
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-09-27 16:50:58 +0800

    refactor(core): migrate user table schema

--- packages/suite/src/constants.ts
+++ packages/suite/src/constants.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://api.getsuite.io';
-export const API_URL = 'http://localhost:8888';
+export const API_URL = 'https://api.getsuite.io';
+// export const API_URL = 'http://localhost:8888';
